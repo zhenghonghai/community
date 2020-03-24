ca3651649e14bc89d4a00973efa34ad349cfba72
zhenghonghai <1092548413@qq.com>
2020-03-25 00:53:35 +0800

    add 分页功能

--- src/main/java/com/redsea/community/controller/IndexController.java
+++ src/main/java/com/redsea/community/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.redsea.community.controller;
 
+import com.redsea.community.dto.PaginationDTO;
 import com.redsea.community.dto.QuestionDTO;
 import com.redsea.community.mapper.QuestionMapper;
 import com.redsea.community.mapper.UserMapper;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -26,7 +28,10 @@ public class IndexController {
     private QuestionService questionService;
 
     @GetMapping("/")
-    public String index(HttpServletRequest request, Model model) {
+    public String index(HttpServletRequest request, Model model,
+                        @RequestParam(name = "page", defaultValue = "1")Integer page,
+                        @RequestParam(name = "size", defaultValue = "5")Integer size
+                        ) {
         Cookie[] cookies = request.getCookies();
         if (AssertUtil.isNotEmpty(cookies)) {
             for (Cookie cookie : cookies) {
@@ -40,8 +45,8 @@ public class IndexController {
                 }
             }
         }
-        List<QuestionDTO> questionList = questionService.list();
-        model.addAttribute("questions", questionList);
+        PaginationDTO pagination = questionService.list(page,size);
+        model.addAttribute("pagination", pagination);
         return "index";
     }
 }
--- src/main/java/com/redsea/community/service/QuestionService.java
+++ src/main/java/com/redsea/community/service/QuestionService.java
@@ -1,5 +1,6 @@
 package com.redsea.community.service;
 
+import com.redsea.community.dto.PaginationDTO;
 import com.redsea.community.dto.QuestionDTO;
 import com.redsea.community.mapper.QuestionMapper;
 import com.redsea.community.mapper.UserMapper;
@@ -21,8 +22,13 @@ public class QuestionService {
     @Autowired
     private QuestionMapper questionMapper;
 
-    public List<QuestionDTO> list() {
-        List<Question> questions = questionMapper.list();
+    public PaginationDTO list(Integer page, Integer size) {
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalCount = questionMapper.count();
+        paginationDTO.setPagination(totalCount,page,size);
+
+        Integer offset = size * (paginationDTO.getPage() - 1);
+        List<Question> questions = questionMapper.list(offset, size);
         List<QuestionDTO> questionDTOS = new ArrayList<>();
         for (Question question : questions) {
             User user = userMapper.findById(question.getCreator());
@@ -31,6 +37,7 @@ public class QuestionService {
             questionDTO.setUser(user);
             questionDTOS.add(questionDTO);
         }
-        return questionDTOS;
+        paginationDTO.setQuestions(questionDTOS);
+        return paginationDTO;
     }
 }
