e49a2239afe4b1a5133436203edd03dede797ee7
zhenghonghai <1092548413@qq.com>
2020-03-26 16:19:12 +0800

    完成详情页面

---
+++ src/main/java/com/redsea/community/controller/QuestionController.java
@@ -0,0 +1,24 @@
+package com.redsea.community.controller;
+
+import com.redsea.community.dto.QuestionDTO;
+import com.redsea.community.mapper.QuestionMapper;
+import com.redsea.community.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class QuestionController {
+
+    @Autowired
+    private QuestionService questionService;
+
+    @GetMapping("/question/{id}")
+    public String question(@PathVariable("id") Integer id, Model model) {
+        QuestionDTO questionDTO = questionService.getById(id);
+        model.addAttribute("question", questionDTO);
+        return "question";
+    }
+}
